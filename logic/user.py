ffaae9245108fca28dce7887703f55717664c5bc
kelvin <kelvinjyoga@gmail.com>
2025-03-14 22:27:37 +1100

    updates on user

--- logic/user.py
+++ logic/user.py
@@ -7,14 +7,24 @@ database = client['connisewer']
 collection = database['toilets']
 
 class User:
-    def __init__(self, name, id):
+    # name = string
+    # id = string
+    # email = string
+    # profile_pic = ????
+    # reviews = array
+    # ratings = array
+    def __init__(self, name, id, profile_pic, email, reviews, ratings):
         self.id = id
         self.name = name
+        self.profile_pic = profile_pic
+        self.email = email
+        self.arrayReviews = reviews
+        self.arrayRatings = ratings
 
 
 # add user
-def addUser(name, id):
-    user = User(name, id)
-    collection.insert_one(user.to_dict())
+def addUser(name, id, profile_pic, email, reviews, ratings):
+    user = User(name, id, profile_pic, email, reviews, ratings) # make user
+    collection.insert_one(user.to_dict())   # add user to collection/database
 
     
